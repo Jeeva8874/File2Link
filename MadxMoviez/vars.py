022fef13aa01c42eb7afcac29d57bdd0b8f196ff
Jeeva8874 <110565010+Jeeva8874@users.noreply.github.com>
2024-12-19 23:30:55 +0530

    Update vars.py

--- MadxMoviez/vars.py
+++ MadxMoviez/vars.py
@@ -10,18 +10,18 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv("API_ID", ""))
-    API_HASH = str(getenv("API_HASH", ""))
-    BOT_TOKEN = str(getenv("BOT_TOKEN", ""))
+    API_ID = int(getenv("API_ID", "22977776"))
+    API_HASH = str(getenv("API_HASH", "2ac7223d720bdeec757cbc88ced57224"))
+    BOT_TOKEN = str(getenv("BOT_TOKEN", "7870220937:AAHKH5QJYqtpGTZuNpx7qOPVvUpwBgVy-aE"))
     name = str(getenv("name", "filetolinkbot"))
     SLEEP_THRESHOLD = int(getenv("SLEEP_THRESHOLD", "60"))
     WORKERS = int(getenv("WORKERS", "200"))
     CUSTOM_FILE_CAPTION = environ.get(
         "CUSTOM_FILE_CAPTION", "<b>ɴᴀᴍᴇ : {file_name}\n\nꜱɪᴢᴇ : {file_size}</b>"
     )
-    BIN_CHANNEL = int(getenv("BIN_CHANNEL", "-1014309"))
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-100309"))
-    PERMANENT_GROUP = os.environ.get("PERMANENT_GROUP", "-1014309")
+    BIN_CHANNEL = int(getenv("BIN_CHANNEL", "-1002480489590"))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002480489590"))
+    PERMANENT_GROUP = os.environ.get("PERMANENT_GROUP", "-1002480489590")
 
     CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001981587599"))
 
@@ -37,20 +37,20 @@ class Var(object):
     SHORTLINK_API2 = os.environ.get(
         "SHORTLINK_API2", "4bdcdaa51a263ea5a2bec5541cdec58f922e414c"
     )
-    LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", "-10022790"))
-    FORCE_SUB = os.environ.get("FORCE_SUB", "")
-    PORT = int(getenv("PORT", "7575"))
+    LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", "-1002391269521"))
+    FORCE_SUB = os.environ.get("FORCE_SUB", "-1002159407577")
+    PORT = int(getenv("PORT", "80"))
     BIND_ADRESS = str(getenv("WEB_SERVER_BIND_ADDRESS", "0.0.0.0"))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))
     ADMIN = [
         int(admin) if id_pattern.search(admin) else admin
-        for admin in os.environ.get("ADMIN", "1032438381").split()
+        for admin in os.environ.get("ADMIN", "6762558871").split()
     ]
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1032438381").split())
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "6762558871").split())
     NO_PORT = bool(getenv("NO_PORT", False))
     APP_NAME = str(getenv("APP_NAME", "mf2l.madxbotz"))
-    OWNER_USERNAME = str(getenv("OWNER_USERNAME", "@RUBAN9124"))
-    BOT_USERNAME = str(getenv("BOT_USERNAME", "f2luhd_bot"))
+    OWNER_USERNAME = str(getenv("OWNER_USERNAME", "@Heart_thieft"))
+    BOT_USERNAME = str(getenv("BOT_USERNAME", "HeartxF2L_Bot"))
     if "DYNO" in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv("APP_NAME", "mf2l.madxbotz"))
@@ -79,5 +79,5 @@ class Var(object):
     )
     UPDATES_CHANNEL = str(getenv("UPDATES_CHANNEL", "None"))
     BANNED_CHANNELS = list(
-        set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split())
+        set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1002431206698")).split())
     )
